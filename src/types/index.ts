b727de55275cb37a05291c324a47b97b4708c6ab
Leon <qinlanfeng@foxmail.com>
2020-08-28 01:52:40 +0800

    feat: 获取响应数据
    获取响应数据;增加获取响应数据demo

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,10 +14,24 @@ export type Method =
   | 'patch'
   | 'PATCH'
 
+export type XMLHttpRequestResponseType = '' | 'arraybuffer' | 'blob' | 'document' | 'json' | 'text'
+
 export interface AxiosRequestConfig {
   url: string
   method?: Method
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
 }
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
