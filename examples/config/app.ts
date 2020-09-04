cf962200fe9f1b00d1d74159e9b0b451115b0b06
Leon <qinlanfeng@foxmail.com>
2020-09-05 00:10:34 +0800

    feat: 请求响应和响应配置化;扩展axios.create静态接口

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,4 +1,4 @@
-import axios from "../../src";
+import axios, { AxiosTransformer } from "../../src";
 import qs from 'qs'
 
 axios.defaults.headers.common['test2'] = 123
@@ -13,5 +13,46 @@ axios({
     test: '321'
   }
 }).then(res => {
+  console.log(res.data)
+})
+
+axios({
+  transformRequest: [(function(data) {
+    return qs.stringify(data)
+  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+    if (typeof data === 'object') {
+      data.b = 2
+    }
+    return data
+  }],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then((res) => {
+  console.log(res.data)
+})
+
+const instance = axios.create({
+  transformRequest: [(function(data) {
+    return qs.stringify(data)
+  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), (function(data) {
+    if (typeof data === 'object') {
+      data.c = 3
+    }
+    return data
+  })]
+})
+
+instance({
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then((res) => {
   console.log(res.data)
 })
