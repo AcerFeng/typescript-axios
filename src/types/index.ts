cf4b7e9662eca9a02790e5ed1e591a007ceead83
Leon <qinlanfeng@foxmail.com>
2020-09-11 02:01:48 +0800

    feat: baseURL实现

--- src/types/index.ts
+++ src/types/index.ts
@@ -98,6 +98,7 @@ export interface AxiosRequestConfig {
   auth?: AxiosBasicCredentials
   validateStatus?: (status: number) => boolean
   paramsSerializer?: (params: any) => string
+  baseURL?: string
   [propName: string]: any
 }
 
